95be85dab699370275cea21ebc9422c36d6b86d1
Rodrigo Barba <a20490687@itmexicali.edu.mx>
2023-10-13 21:49:45 -0700

    New: Login Base.

---
+++ third-project/js/script-dashboard.js
@@ -0,0 +1,14 @@
+// Verificar si el usuario ha iniciado sesión
+var authenticatedUser = localStorage.getItem("authenticatedUser");
+
+if (!authenticatedUser) {
+    // El usuario no ha iniciado sesión, redirige a la página de error
+    window.location.href = "../html/error.html";
+}
+
+// Establecer un temporizador de 10 segundos para cerrar la sesión
+setTimeout(function () {
+    // Limpiar la sesión local y redirigir a la página de inicio de sesión
+    localStorage.removeItem("authenticatedUser");
+    window.location.href = "../html/index.html";
+}, 10000); // 10000 milisegundos = 10 segundos
---
+++ third-project/js/script-error.js
@@ -0,0 +1,4 @@
+function reback() {
+    // Redirige al usuario a la página de inicio de sesión
+    window.location.href = ".../html/index.html";
+}
